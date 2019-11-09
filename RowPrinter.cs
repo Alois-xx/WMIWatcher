afb1546f7d058986f3053216f7df466d74d1a8bc
Alois Kraus <akraus1@gmx.de>
2019-11-09 23:17:54 +0100

    1.0.1 Some queries did contain newlines which did distort the csv output. Removed superfluous characters

--- RowPrinter.cs
+++ RowPrinter.cs
@@ -6,12 +6,15 @@ namespace WMIWatcher
 {
     class Row
     {
+        static char[] myTrimChars = new char[] { '\r', '\n' };
+
         public static string Print(params string[] strings)
         {
             StringBuilder sb = new StringBuilder();
             for(int i=0;i<strings.Length;i++)
             {
-                sb.Append(strings[i]);
+                // prevent multiline rows and replace any column separators with #
+                sb.Append(strings[i]?.Trim(myTrimChars).Replace('|','#'));
                 if( i != strings.Length-1)
                 {
                     sb.Append("|");
